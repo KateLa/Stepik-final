c96a8633380c0ef0ccb8bfdd35adc6df52a5121e
Kate <kate.lazovskaya@gmail.com>
2019-06-29 23:47:26 +0300

    product page locators added

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators(object):
 class LoginPageLocators(object):
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "register_form")
+    
+class ProductPageLocators(object):
+    ADD_BUTTON = (By.CLASS_NAME, 'btn-add-to-basket')
+    LANG_EN_GB = (By.XPATH, '//html')
+    SUCCESS_ADDITION_ALERT = (By.CSS_SELECTOR, '.alert-success')
+    ALERT_PRICE = (By.CSS_SELECTOR, '.alert-info strong')
+    PRODUCT_NAME_ADDITION_ALERT = (By.CSS_SELECTOR, '.alert-success strong')
