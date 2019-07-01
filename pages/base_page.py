9047d7a280b9371522eb6ec6213714494115147d
Kate <kate.lazovskaya@gmail.com>
2019-07-01 11:21:16 +0300

    update base page

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,9 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
+import math
 
 class BasePage(object):
     def __init__(self, browser, url, timeout=10):
@@ -11,6 +14,13 @@ class BasePage(object):
 
     def open(self):
         self.browser.get(self.url)
+        
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+    
+    def should_be_login_link(self):
+        assert self.is_element_present (*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_element_present(self, how, what):
         try:
@@ -18,6 +28,7 @@ class BasePage(object):
         except NoSuchElementException:
             return False
         return True
+    
     def is_not_element_present(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
@@ -31,3 +42,12 @@ class BasePage(object):
         except TimeoutException:
             return False
         return True
+    
+    def go_to_cart_page(self):
+        link = self.browser.find_element(*BasePageLocators.VIEW_CART_BUTTON)
+        link.click()
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                 " probably unauthorised user"
+    
