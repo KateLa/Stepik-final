a82b1cb43f3c239cac1bdb428c8bb95fc113eaa9
Kate <kate.lazovskaya@gmail.com>
2019-07-01 11:23:15 +0300

    base page locators added

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,23 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators(object):
+class BasePageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    VIEW_CART_BUTTON = (By.CSS_SELECTOR, "div.basket-mini a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+    
 class LoginPageLocators(object):
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "register_form")
+    REGISTRATION_EMAIL = (By.ID, "id_registration-email")
+    REGISTRATION_PASSWORD_1 = (By.ID, "id_registration-password1")
+    REGISTRATION_PASSWORD_2 = (By.ID, "id_registration-password2")
+    REGISTRATION_BUTTON = (By.NAME, "registration_submit")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
     
+class MainPageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+  
 class ProductPageLocators(object):
     ADD_BUTTON = (By.CLASS_NAME, 'btn-add-to-basket')
     LANG_EN_GB = (By.XPATH, '//html')
